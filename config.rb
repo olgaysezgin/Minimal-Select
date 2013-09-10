43ad24f8e41af941c9cae7d1f02cdc5f6c69c526
Olgay Sezgin <olgaysezgin@gmail.com>
2013-09-10 14:17:33 +0300

    implemented beta select

---
+++ config.rb
@@ -0,0 +1,8 @@
+http_path = ""
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+line_comments = false
+cache = false
+output_style = :compressed
---
+++ js/minimal-select.js
@@ -0,0 +1,171 @@
+/*
+ *	Minimal Select
+ *	Requires jQuery library (http://www.jquery.com) and jQuery Class plug-in
+ *	Olgay Sezgin (olgay at hipo dot biz, @olgaysezgin) - September 10, 2013
+ */
+
+
+$.namespace("MinimalSelect.UiSelect");
+
+MinimalSelect.UiSelect = $.Class.extend({
+
+    SELECT : '',
+    option_values : [],
+    option_texts : [],
+    selected_option : -1,
+
+    select_id : '',
+    select_template : '',
+    placeholder_text : '',
+    list_top : 42,
+    lit_max_height: 156,
+
+    init : function (options) {
+
+        $.extend(this, options);
+
+        this.get_options();
+        this.prepare_uiselect();
+        $('body').click(this.click_body.bind(this));
+
+    },
+
+    hide_select : function () {
+
+        $(this.SELECT).hide();
+
+    },
+
+    click_body : function (event) {
+
+        if ( !$(event.target).parents(this.select_id).is('.m-select') ) {
+            if ( $(this.select_id).is('.active') ) {
+                this.toggle_list();
+            }
+            
+            $(this.select_id).find('.m-placeholder').removeClass('focused');
+        }
+
+    },
+
+    get_options : function () {
+
+        $(this.SELECT).find('option').each(function (i, e) {
+            
+            var attr = $(e).attr('selected');
+            if ( typeof attr !== 'undefined' && attr !== false ) {
+                this.selected_option = i;
+            }
+
+            this.option_values.push($(e).val());
+            this.option_texts.push($(e).text());
+            
+        }.bind(this));
+
+    },
+
+    get_placeholder : function () {
+
+        this.placeholder_text = $(this.SELECT).attr('placeholder');
+
+    },
+
+    create_uiselect_id : function () {
+
+        this.select_id = '#mselect-' + this.SELECT.replace('#', '');
+
+    },
+
+    prepare_template : function () {
+
+        this.get_placeholder();
+        this.create_uiselect_id();
+        this.select_template = '<div id="' + this.select_id.replace('#', '') + '" class="m-select"><b></b><span class="m-placeholder" title="' + this.placeholder_text + '">' + this.placeholder_text + '</span><ul class="m-list" style="max-height: ' + this.lit_max_height +';top:' + (this.list_top + 30) + '; opacity:0"></ul></div>';
+
+    },
+
+    prepare_uiselect : function () {
+
+        this.prepare_template();
+        $(this.SELECT).after(this.select_template);
+        this.set_select_width();
+        this.hide_select();
+        this.prepare_list();
+        $(this.select_id).find('.m-placeholder').click(this.toggle_list.bind(this));
+        $(this.select_id).find('a').click(this.select_option.bind(this));
+
+    },
+
+    prepare_list : function () {
+
+        for (var i = 0; i <= this.option_texts.length -1; i++) {
+
+            var item = '<li><a href="javascript:void(0);">' + this.option_texts[i] + '</a></li>';
+            $(this.select_id).find('ul.m-list').append(item);
+
+            // if ( this.selected_option == i ) {
+            //     this.change_placeholder(this.option_texts[i]);
+            // }
+
+            if ( i == 0 ) {
+                $(this.select_id).find('ul.m-list li a').addClass('selected');
+            }
+            
+        };
+
+    },
+
+    toggle_list : function () {
+
+        var list = $(this.select_id).find('ul.m-list');
+
+        if ( $(this.select_id).is('.active') ) {
+            
+            list.stop().animate({
+                opacity: 0,
+                top: (this.list_top + 30)
+            }, 150, function(){
+                list.hide();
+                $(this.select_id).removeClass('active');
+            }.bind(this));
+
+        } else {
+            
+            $(this.select_id).find('.m-placeholder').addClass('focused');
+
+            list.stop().show().animate({
+                opacity: 1,
+                top: this.list_top
+            }, 150, function(){
+                $(this.select_id).addClass('active');
+            }.bind(this));
+
+        }
+
+    },
+
+    select_option : function (event) {
+
+        var clicked_item = $(event.target).parent('li').index();
+        $(this.SELECT).find('option[value="' + this.option_values[clicked_item] + '"]').attr('selected', true);
+        this.change_placeholder(this.option_texts[clicked_item]);
+        this.toggle_list();
+
+    },
+
+    change_placeholder : function (value) {
+
+        $(this.select_id).find('.m-placeholder').text(value);
+
+    },
+
+    set_select_width : function () {
+
+        var select_width = $(this.SELECT).width() + 40;
+        $(this.select_id).width(select_width);
+        $(this.select_id).find('.m-placeholder').width((select_width - 50));
+        $(this.select_id).find('ul.m-list').width(select_width);
+
+    },
+
+});
